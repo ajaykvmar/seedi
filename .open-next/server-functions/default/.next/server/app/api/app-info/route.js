8750d8559ad1bee46ef068f9f1d2f52af5a75efb
Deepali <deepali@MacBookAir.lan>
2026-05-15 03:03:23 +0530

    fix: migrate from next-on-pages to OpenNext for Cloudflare deployment

---
+++ .open-next/server-functions/default/.next/server/app/api/app-info/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/app-info/route.js")
+R.c("server/chunks/[root-of-the-server]__0jsvgi3._.js")
+R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
+R.c("server/chunks/_next-internal_server_app_api_app-info_route_actions_0.1hah~.js")
+R.m(54662)
+module.exports=R.m(54662).exports
---
+++ .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_tracker_new_page_actions_0nn7djd.js
@@ -0,0 +1,3 @@
+module.exports=[96139,(a,b,c)=>{}];
+
+//# sourceMappingURL=_next-internal_server_app_tracker_new_page_actions_0nn7djd.js.map
