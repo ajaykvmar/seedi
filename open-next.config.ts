50c133fd48c67b217a77e2ce2f5822dc5929b1fb
Deepali <deepali@MacBookAir.lan>
2026-05-15 03:42:52 +0530

    fix: remove KV cache dependency from OpenNext config

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kv from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kv,
-});
+export default defineCloudflareConfig({});
